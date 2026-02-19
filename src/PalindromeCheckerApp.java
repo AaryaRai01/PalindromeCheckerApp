508f29802bcdcece95e766a746afb87200f6a9c5
Aarya Rai <aarya.rai185@gmail.com>
2026-02-19 10:01:57 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input Text: ");
+        String word = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println("Is it a Palindrome? True");
+        } else {
+            System.out.println("Is it a Palindrome? False");
+        }
+
+        // Close scanner
+        scanner.close();
+
 
     }
 }
